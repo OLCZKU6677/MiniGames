64fa212bf42cf0a171a61c6d85abc89d2b9d122d
OLCZKU6677 <olczkuyt@gmail.com>
2026-07-21 16:14:54 +0200

    Fix FoliaLib dependency repository

--- build.gradle.kts
+++ build.gradle.kts
@@ -9,6 +9,7 @@ repositories {
     maven("https://repo.papermc.io/repository/maven-public/")
     maven("https://repo.panda-lang.org/releases")
     maven("https://repo.okaeri.cloud/releases")
+    maven("https://repo.nexomc.com/releases/")
 }
 
 dependencies {
@@ -41,6 +42,7 @@ tasks {
         archiveClassifier.set("")
         relocate("dev.rollczi.litecommands", "pl.olczku.miniGames.libs.litecommands")
         relocate("me.devnatan.inventoryframework", "pl.olczku.miniGames.libs.inventoryframework")
+        relocate("com.tcoded.folialib", "pl.olczku.miniGames.libs.folialib")
         relocate("eu.okaeri.configs", "pl.olczku.miniGames.libs.okaericonfigs")
     }
     build { dependsOn(shadowJar) }
