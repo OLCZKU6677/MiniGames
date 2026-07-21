250d722eadaaaa881a3a0b49de1de389ef74f08e
OLCZKU6677 <olczkuyt@gmail.com>
2026-07-21 15:57:42 +0200

    Initial commit

---
+++ src/main/java/pl/olczku/miniGames/MiniGames.java
@@ -0,0 +1,16 @@
+package pl.olczku.miniGames;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class MiniGames extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
